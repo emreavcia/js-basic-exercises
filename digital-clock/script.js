9e10a992c39bfff816809611a49e575a8c4329c5
Emre Avci <84312074+emreavcia@users.noreply.github.com>
2022-02-23 00:01:19 +0100

    file is edited, some mistakes are corrected

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,30 +1,30 @@
-function showTime(){
+function showTime() {
 
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var session = "AM";
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
 
-    if(h == 0){
-        h=12
+    if (h == 0) {
+        h = 12
     }
-    if(h>12){
-        h-=12;
+    if (h > 12) {
+        h -= 12;
         session = "PM";
     }
 
-    h= (h<10) ? "0" + h : h;
-    m= (m<10) ? "0" + m : h;
-    s= (s<10) ? "0" + s : h;
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
-    var time = h + ":" + m + ":" + s + " " + session;
+    let time = h + ":" + m + ":" + s + " " + session;
 
     document.getElementById("clock").innerText = time;
     /* document.getElementById("clock").textContent = time; */
 
     setTimeout(showTime, 1000);
-    
+
 };
 
 showTime();
